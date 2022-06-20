72023187e64bcac01908004919c7f12ad234fe4c
Nate (1x2kb) <1x2kb@users.noreply.github.com>
2022-06-19 22:59:36 -0500

    Refactor code to use stdin lines (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,70 @@
-use std::io::Error;
+use std::io::{BufRead, Error, Lines, StdinLock, StdoutLock, Write};
 
-fn main() {
-    let lines = read_full_stdin()
-        .unwrap()
-        .into_iter()
-        .reduce(|a, b| a + " " + &b)
-        .unwrap();
-
-    println!("{}", lines);
+enum WriteType {
+    Single,
+    Mutli,
 }
 
-fn read_full_stdin() -> Result<Vec<String>, Error> {
-    let mut matching_lines = vec![];
+fn main() {
+    let matching_sequence = "origin/"; // TODO: read in from env::args.
+    let mode_type = WriteType::Single; // TODO: read mode type from env::args.
+    let lines = get_lines_from_stdin();
+    let matches = scan_buffer_for_matches(lines, matching_sequence).unwrap();
 
-    let mut read = 1;
+    write_matches_to_output(matches, mode_type).unwrap();
+}
 
-    let match_sequence = "origin/";
+fn get_lines_from_stdin() -> Lines<StdinLock<'static>> {
+    std::io::stdin().lock().lines()
+}
 
-    while read != 0 {
-        let mut line = String::new();
-        read = std::io::stdin().read_line(&mut line)?;
-        let line = line.trim();
+fn scan_buffer_for_matches(
+    lines: Lines<StdinLock>,
+    match_sequence: &str,
+) -> Result<Vec<String>, Error> {
+    let matching_lines = lines
+        .into_iter()
+        .filter_map(|line_result| match line_result {
+            Ok(line) => {
+                if let Some(matched_index) = line.find(match_sequence) {
+                    return Option::Some(
+                        line.get(matched_index + match_sequence.len()..line.len())
+                            .unwrap()
+                            .to_string(),
+                    );
+                }
 
-        if let Some(result) = line.find(match_sequence) {
-            println!("Found origin/ at location: {}", result);
-
-            let line: String = line
-                .get(result + match_sequence.len()..line.len())
-                .unwrap()
-                .into();
-            println!("Line without text: {}", line);
-            matching_lines.push(line);
-        }
-    }
+                return Option::None;
+            }
+            Err(_) => Option::None,
+        })
+        .collect();
 
     Result::Ok(matching_lines)
+}
+
+fn write_matches_to_output(matches: Vec<String>, mode: WriteType) -> Result<(), Error> {
+    let mut output = std::io::stdout().lock();
+
+    match mode {
+        WriteType::Single => write_single_line(matches, output),
+        WriteType::Mutli => write_multi_line(matches, output),
+    }
+}
+
+fn write_single_line(matches: Vec<String>, mut output: StdoutLock) -> Result<(), Error> {
+    let line = matches.into_iter().reduce(|a, b| a + " " + &b).unwrap();
+
+    output.write(line.as_bytes()).unwrap();
+    output.flush()
+}
+
+fn write_multi_line(matches: Vec<String>, mut output: StdoutLock) -> Result<(), Error> {
+    let output_copy = &mut output;
+
+    matches.into_iter().for_each(move |matched_line| {
+        output_copy.write(matched_line.as_bytes()).unwrap();
+    });
+
+    output.flush()
 }
